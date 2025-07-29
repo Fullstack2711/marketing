f68f5b4b5fd1d887bf83d5ba6c2f72827f81e651
Fullstack2711 <ibragimovzafar0001@gmail.com>
2025-07-29 17:15:03 +0500

    line animation position fixeddan absolutega otqazi,di

--- app/page.js
+++ app/page.js
@@ -65,7 +65,7 @@ export default function Home() {
           {/* HEADER qismi */}
           <div className="header   min-h-screen relative  overflow-hidden">
             {/* SVG chiziqli animatsiya - faqat mobil uchun */}
-            <div className="fixed top-0 lg:-top-10 left-1/2 -translate-x-1/2  w-full pointer-events-none z-10">
+            <div className="absolute top-0 lg:-top-10 left-1/2 -translate-x-1/2  w-full pointer-events-none z-10">
               <Image
                 src="/Lineanimation.svg"
                 alt="Decorative lines"
